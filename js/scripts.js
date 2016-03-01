367d27ea05ab7ece58c188bb46c73e9a3316f0dd
parkercatalano <parkercatalano@gmail.com>
2016-02-29 20:55:57 -0800

    update to end of video on pig

--- js/scripts.js
+++ js/scripts.js
@@ -7,32 +7,16 @@
 //})
 
 var pigLatin = function(engWord) {
-  if(!hasConsonantAt(engWord,0)) {
-    var transWord = engWord;
-  } else if (!hasConsonantAt(engWord, 1)) {
-    var transWord = engWord.slice(2) + engWord.slice(0,2);
-  } else if(hasConsonantAt(engWord,0)) {
-    var transWord = engWord.slice(1) + engWord.slice(0,1);
-  }
+  var splitAt = indexOfFirstVowel(engWord);
+  var transWord = engWord.slice(splitAt) + engWord.slice(0, splitAt);
   return transWord + 'ay';
 };
 
-var hasConsonantAt = function(word, position) {
-  var vowels = ['a','e','i','o','u'];
-  for (var i = 0; i < vowels.length; i++) {
-    if (word[position] === vowels[i]) {
-      return false;
-    }
-}
-return true
-};
-
 var indexOfFirstVowel = function(word) {
-  var index = undefined;
   var vowels = ['a','e','i','o','u'];
   var letters = word.split("");
+
   for (var i = 0;i < letters.length; i++){
-    var letter = letters[i];
     for (var j = 0; j< vowels.length; j++) {
       if (letters[i] === vowels[j]) {
         return index = i;
